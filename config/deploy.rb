18f880947d8bf8b41b5e5e2f3816ca56e448eb31
lukas <lukas.bitter@gmail.com>
2016-02-17 09:30:34 +0100

    configure git repo in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'app'
-set :repo_url, 'git@github.com:HE-Arc/demo-rails-application.git'
+set :repo_url, 'git@github.com:LukasBitter/demo-rails-application.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
